8e20fa5c004f53fd13aa476a10f765e65503f679
ekinkoseoglu <ekindenizkoseoglu@gmail.com>
2022-02-18 12:38:43 +0300

    (Addition out of Sessions) Add Error Handling in Service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,16 +1,37 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ProductResponseModel } from '../models/ProductResponseModel';
-import { Observable } from 'rxjs';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  apiUrl: string = 'https://localhost:44314/api/products/getall';
+  apiUrl: string = 'https://localhost:44360/api/products/getall';
   constructor(private httpClient: HttpClient) {}
+
+  private handleError(err: HttpErrorResponse) {
+    if (err.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An Error has occurred: ' + err.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        'Backend returned code ${err.status}, body was: ' + err.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened, please try again later')
+    );
+  }
 
   getProducts(): Observable<ProductResponseModel> {
     return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+    // .pipe(
+    // retry(3), /* Retry a failed request up to 3 times */
+    // catchError(this.handleError)
+    // );
   }
 }
